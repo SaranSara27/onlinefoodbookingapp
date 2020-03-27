a8a888480423c541e90fa2b52e3845a3bbefc41c
ubuntu <ubuntu@ip-172-31-95-151.ec2.internal>
2020-03-27 09:29:10 +0000

    Initial commit

---
+++ src/main/java/com/cts/foodbooking/application/Booking.java
@@ -0,0 +1,14 @@
+package com.cts.foodbooking.application;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Booking {
+
+	final static Logger log = LoggerFactory.getLogger(Booking.class);
+
+	public static void main(String[] args) {
+
+	}
+
+}
