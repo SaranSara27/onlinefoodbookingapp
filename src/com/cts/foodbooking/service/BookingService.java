50ea0796115ca164fab40458947584d335798145
ubuntu <ubuntu@ip-172-31-95-151.ec2.internal>
2020-03-29 09:59:06 +0000

    Modified test case

--- src/com/cts/foodbooking/service/BookingService.java
+++ src/com/cts/foodbooking/service/BookingService.java
@@ -5,13 +5,21 @@ import java.time.LocalTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.cts.foodbooking.application.Booking;
 import com.cts.foodbooking.dao.BookingDAO;
 import com.cts.foodbooking.model.Restaurant;
 
+
 public class BookingService {
+	
+	final static Logger log = LoggerFactory.getLogger(Booking.class);
 
 	public static String fetchRestaurantDetails() {
 		List<Restaurant> resList = BookingDAO.fetchRestaurantDetails();
+		log.debug("resList in fetchRestaurantDetails ={}",resList);
 		if (resList == null) {
 			return "failure";
 		}
@@ -19,27 +27,31 @@ public class BookingService {
 	}
 
 	public static String ratingBasedRestaurant(int rating) {
-		List<Restaurant> resList = BookingDAO.fetchRestaurantDetails().stream().filter(r -> r.getRatings() < 4)
+		List<Restaurant> list = BookingDAO.fetchRestaurantDetails().stream().filter(r -> r.getRatings() < rating)
 				.collect(Collectors.toList());
-		if (resList == null) {
+		log.debug("resList in ratingBasedRestaurant={}",list);
+		if (list == null) {
 			return "failure";
 		}
-		return BookingDAO.writeRatingBasedRestaurant(resList);
+		return BookingDAO.writeRatingBasedRestaurant(list);
 	}
 
 	public static String timingBasedRestaurant(LocalTime startTime, Duration frequency) {
 
-		List<Restaurant> resList = BookingDAO.fetchRestaurantDetails().stream()
+		List<Restaurant> restaurantList = BookingDAO.fetchRestaurantDetails().stream()
 				.filter(t -> filterTime(t, startTime, frequency)).collect(Collectors.toList());
-		if (resList == null) {
+		log.debug("resList in timingBasedRestaurant={}",restaurantList);
+		if (restaurantList == null) {
 			return "failure";
 		}
-		return BookingDAO.writeTimingBasedRestaurant(resList);
+		return BookingDAO.writeTimingBasedRestaurant(restaurantList);
 
 	}
 
 	private static boolean filterTime(Restaurant res, LocalTime startTime, Duration frequency) {
+		log.debug("startTime {}",startTime);
 		LocalTime endTime = startTime.plusHours(frequency.toHours());
+		log.debug("endTime {}",endTime);
 		if ((res.getOpeningTime().equals(startTime) || res.getOpeningTime().isBefore(startTime))
 				&& (res.getClosingTime().equals(endTime) || res.getClosingTime().isAfter(endTime))) {
 			return true;
--- src/com/cts/foodbooking/test/BookingServiceTest.java
+++ src/com/cts/foodbooking/test/BookingServiceTest.java
@@ -5,18 +5,12 @@ import static org.junit.Assert.assertEquals;
 import java.time.Duration;
 import java.time.LocalTime;
 
-import org.junit.Before;
 import org.junit.Test;
-//import org.mockito.MockitoAnnotations;
 
 import com.cts.foodbooking.service.BookingService;
 
 public class BookingServiceTest {
 	
-	/*
-	 * @Before public void setup() { MockitoAnnotations.initMocks(this); }
-	 */
-
 	@Test
 	public void fetchRestaurantDetailsTest() {
 		assertEquals("Success", BookingService.fetchRestaurantDetails());
@@ -24,11 +18,11 @@ public class BookingServiceTest {
 	
 	@Test
 	public void ratingBasedRestaurantTest() {
-		assertEquals("Success", BookingService.ratingBasedRestaurant(3));
+		assertEquals("Success", BookingService.ratingBasedRestaurant(4));
 	}
 	
 	@Test
 	public void timingBasedRestaurantTest() {
-		assertEquals("Success", BookingService.timingBasedRestaurant(LocalTime.of(12, 00),Duration.ofHours(1)));
+		assertEquals("Success", BookingService.timingBasedRestaurant(LocalTime.of(10, 00),Duration.ofHours(1)));
 	}
 }
